487d420948e5a6be34f0870952bba13ab682ee4b
Bastos, Lucas <lucascorte13@gmail.com>
2026-06-12 18:58:06 -0300

    Add keep-alive self-ping to prevent Render spin-down

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT || 8080;
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Keep-alive ping endpoint
+app.get('/ping', (req, res) => {
+    res.status(200).send('pong');
+});
+
+// Self-ping every 14 minutes to prevent Render free tier spin-down
+setInterval(() => {
+    const url = process.env.RENDER_EXTERNAL_URL || `http://localhost:${PORT}`;
+    fetch(`${url}/ping`).catch(() => {});
+}, 14 * 60 * 1000);
+
 app.listen(PORT, () => {
     console.log(`Yeah Buddy! server running on port ${PORT}`);
 });
